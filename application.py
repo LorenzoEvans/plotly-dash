c4586327cb2e24f654258ae04a14a596cf3b2daf
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-03-26 12:43:23 -0700

    broke children out, setup layout

--- application.py
+++ application.py
@@ -11,3 +11,24 @@ application = app.server
 app.title = 'Dash on AWS EB!'
 
 # Set up layout
+
+html_children = [
+  html.H1(children='Hello Dash!'),
+  html.Div(children=''' This Dash is running on Elastic Beanstalk '''),
+  dcc.Graph(
+    id= 'example-graph',
+    figure={
+      'data': [
+        {'x': ['left', 'center', 'right'], 'y': [3,7,6], 'type': 'bar', 'name': 'category 1'},
+        {'x': ['left', 'center', 'right'], 'y': [4,2,5], 'type': 'bar', 'name': 'category 2'},
+      ],
+      'layout': {
+        'plot_bgcolor': 'lightgray',
+        'title': 'Graph Title',
+        'xaxis': {'title': 'x-axis label'},
+        'yaxis': {'title': 'y-axis label'}
+      }
+    }
+  )
+]
+app.layout = html.Div(children=html_children)
